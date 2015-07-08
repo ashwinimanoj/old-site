d235b49d649bc2d66963ea1c7ac589c109e3b2b1
ashwinimanoj <ashwinimanoj@gmail.com>
2015-07-08 23:28:23 +0530

    adding effect

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
- $('div').mouseenter(function(){
+  $('div').mouseenter(function(){
   $('div').fadeTo('fast',1);
  });
- $('div')).mouseleave(function(){
-	$('div').fadeTo('fast',0.5); 
- });
+ 
 });
