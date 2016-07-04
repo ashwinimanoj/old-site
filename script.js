6615311abbc857f54fcce94f535014ad0eb19123
Ashwini Manoj <ashwinimanoj@gmail.com>
2016-07-04 11:32:42 +0530

    Entering project details and some design modifications

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ $(document).ready(function(){
  	$("#about").removeClass("active");
  	$("#projects").removeClass("active");
  	$("#contact").removeClass("active");
-
  	$(window).scrollTo($("section#home"),800);
  });
  
+ $("#to-about").click(function(){
+ 	$("#about").addClass("active");
+ 	$("#home").removeClass("active");
+ 	$("#projects").removeClass("active");
+ 	$("#contact").removeClass("active");
+ 	$(window).scrollTo($("section#about"),800);
+ });
+
  $("#about").click(function(){
  	$("#about").addClass("active");
  	$("#home").removeClass("active");
@@ -17,6 +24,14 @@ $(document).ready(function(){
  	$(window).scrollTo($("section#about"),800);
  });
  
+$("#to-projects").click(function(){
+ 	$("#projects").addClass("active");
+ 	$("#about").removeClass("active");
+ 	$("#home").removeClass("active");
+ 	$("#contact").removeClass("active");
+ 	$(window).scrollTo($("section#projects"),800);
+ });
+
  $("#projects").click(function(){
  	$("#projects").addClass("active");
  	$("#about").removeClass("active");
@@ -25,7 +40,7 @@ $(document).ready(function(){
  	$(window).scrollTo($("section#projects"),800);
  });
  
- $("#contact").click(function(){
+$("#to-contact").click(function(){
  	$("#contact").addClass("active");
  	$("#about").removeClass("active");
  	$("#projects").removeClass("active");
@@ -35,5 +50,21 @@ $(document).ready(function(){
  });
  
 
+ $("#contact").click(function(){
+ 	$("#contact").addClass("active");
+ 	$("#about").removeClass("active");
+ 	$("#projects").removeClass("active");
+ 	$("#home").removeClass("active");
+ 	$(window).scrollTo($("section#contact"),800);
+
+ });
+ 
+ $("#to-home").click(function(){
+ 	$("#home").addClass("active");
+ 	$("#about").removeClass("active");
+ 	$("#projects").removeClass("active");
+ 	$("#contact").removeClass("active");
+ 	$(window).scrollTo($("nav"),800);
+ });
  
 });
